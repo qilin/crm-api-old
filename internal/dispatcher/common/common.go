f033d082b4d423bebc90cbc0f48a1ca68bcbb266
Aleksandr Barsukov <Irioth@gmail.com>
2019-10-21 12:57:45 +0300

    Hasura Authentication (#6)
    Hasura JWT Authentication
    - one shared DB 
    - OAuth via auth1
    - Auto SignIn

--- internal/dispatcher/common/common.go
+++ internal/dispatcher/common/common.go
@@ -1,42 +1,10 @@
 package common
 
-import (
-	"context"
-
-	"github.com/labstack/echo/v4"
-)
-
 const (
 	Prefix       = "internal.dispatcher"
 	UnmarshalKey = "dispatcher"
 
-	// context keys
-	UserContextKey = "dispatcher.ctx.user"
-
 	// routes
-	AuthGroupPath    = "/auth"
 	GraphQLGroupPath = "/v1/graphql"
 	V1Path           = "/v1"
 )
-
-func ExtractUserContext(ctx context.Context) *AuthUser {
-	if user, ok := ctx.Value(UserContextKey).(*AuthUser); ok {
-		return user
-	}
-	return &AuthUser{}
-}
-
-func SetUserContext(ctx echo.Context, user *AuthUser) {
-	ctx.Set(UserContextKey, user)
-}
-
-// AuthUser
-type AuthUser struct {
-	Id    int
-	Email string
-	Roles map[string]bool
-}
-
-func (a *AuthUser) IsEmpty() bool {
-	return len(a.Email) == 0
-}
--- internal/dispatcher/common/config.go
+++ internal/dispatcher/common/config.go
@@ -1,11 +1,5 @@
 package common
 
-type OAuth2 struct {
-	ClientId     string `required:"true"`
-	ClientSecret string `required:"true"`
-	RedirectUrl  string `required:"true"`
-}
-
 type CORS struct {
 	Allowed []string
 	Headers []string
