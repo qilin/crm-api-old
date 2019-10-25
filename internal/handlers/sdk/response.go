32e801d40d3294575dc569fb3d62ffb043bff459
Sergey <serge@cheebo.me>
2019-10-25 21:31:49 +0500

    requests

--- internal/handlers/sdk/response.go
+++ internal/handlers/sdk/response.go
@@ -11,5 +11,30 @@ type AuthResponse struct {
 }
 
 type OrderResponse struct {
-	Data string `json:"data"`
+	// response
+	Version int `json:"version"`
+	// Merchant transaction if
+	TransactionId string `json:"transaction_id"`
+	//
+	OrderId string `json:"order_id"`
+	// Title
+	Title string `json:"title"`
+	// Text description
+	Description string `json:"description"`
+	// URL to image
+	PreviewImage string `json:"preview_image"`
+	// payment amount
+	Amount string `json:"amount"`
+	//
+	Currency string `json:"currency"`
+	// custom data
+	Data Merchant `json:"data"`
+	// timestamp
+	Ts int `json:"ts"`
+}
+
+type Merchant struct {
+	MerchantData string `json:"merchant_data"`
+	// merchant_sing = sha1(merchant_data + merchant_secret_key)
+	MerchantSign string `json:"merchant_sign"`
 }
