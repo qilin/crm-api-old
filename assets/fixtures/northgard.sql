6349860f10bcfabbff45e2e9362f648e1800edee
Aleksandr.Barsukov <irioth@gmail.com>
2019-10-29 14:16:08 +0300

    adapt fixtures to new struct

--- assets/fixtures/northgard.sql
+++ assets/fixtures/northgard.sql
@@ -6,23 +6,36 @@ BEGIN
 	INSERT INTO games (
 		tenant_id,
 		published,
-		name,
-		description,
-		-- tagline,
 		release_date,
 		copyright,
-		genre
+		genre,
+		default_lang
 	)
 	VALUES (
 		1, -- owner organization
 		true, 
-		'Northgard',
-		'Northgard is a strategy game based on Norse mythology in which you control a clan of Vikings vying for the control of a mysterious newfound continent.',
 		'2018.03.07',
 		'(C) 2016 Shiro Games. The Shiro Games name and logo and the Northgard name and logo are trademarks of Shiro Games and may be registered trademarks in certain countries. All rights reserved.',
-		'Indie'
+		'Indie',
+		'en'
 	) 
 	RETURNING id INTO game_id;
+
+	INSERT INTO game_props (
+		id,
+		lang,
+		name,
+		description,
+		-- tagline,
+		site
+	)
+	VALUES (
+		game_id,
+		'en',
+		'Northgard',
+		'Northgard is a strategy game based on Norse mythology in which you control a clan of Vikings vying for the control of a mysterious newfound continent.',
+		'http://northgard.net/'
+	);
 
 	INSERT INTO game_genres 
 		(game_id, genre)
@@ -38,14 +51,14 @@ BEGIN
 		(game_id, 'Achivements');
 
 	INSERT INTO game_developers
-		(game_id, name)
+		(game_id, name, lang)
 	VALUES
-		(game_id, 'Shiro Games');
+		(game_id, 'Shiro Games', 'en');
 
 	INSERT INTO game_publishers
-		(game_id, name)
+		(game_id, name, lang)
 	VALUES
-		(game_id, 'Shiro Games');
+		(game_id, 'Shiro Games', 'en');
 
 	INSERT INTO game_platforms
 		(game_id, platform)
@@ -55,22 +68,22 @@ BEGIN
 		(game_id, 'mac');
 
 	INSERT INTO game_links
-		(game_id, site)
+		(game_id, lang, name, url)
 	VALUES
-		(game_id, 'http://northgard.net/');
+		(game_id, 'en', 'facebook', 'https://www.facebook.com/northgard/');
 
 	INSERT INTO game_languages
 		(game_id, language, interface, sound, subtitles)
 	VALUES
-		(game_id, 'English',             true,  true, true),
-		(game_id, 'French',              true, false, true),
-		(game_id, 'German',              true, false, true),
-		(game_id, 'Russian',             true, false, true),
-		(game_id, 'Simplified Chinese',  true, false, true),
-		(game_id, 'Portuguese - Brazil', true, false, true),
-		(game_id, 'Polish',              true, false, true),
-		(game_id, 'Spanish - Spain',     true, false, true),
-		(game_id, 'Turkish',             true, false, true);
+		(game_id, 'en',         true,  true, true),
+		(game_id, 'fr',         true, false, true),
+		(game_id, 'de',         true, false, true),
+		(game_id, 'ru',         true, false, true),
+		(game_id, 'zh',         true, false, true), -- TODO add zh-Hans
+		(game_id, 'pt-BR',      true, false, true),
+		(game_id, 'pl',         true, false, true),
+		(game_id, 'es-ES',      true, false, true),
+		(game_id, 'tr',         true, false, true);
 
 
 	INSERT INTO game_thumbnails
@@ -79,9 +92,9 @@ BEGIN
 		(game_id, '2x1', 'https://steamcdn-a.akamaihd.net/steam/apps/466560/header.jpg?t=1571828499');
 
 	INSERT INTO game_images
-		(game_id, width, height, url)
+		(game_id, lang, width, height, url)
 	VALUES
-		(game_id, 1920, 1080, 'https://steamcdn-a.akamaihd.net/steam/apps/466560/ss_0de6fea3166a04027b671e438645e98186095c63.jpg?t=1571828499');
+		(game_id, 'en', 1920, 1080, 'https://steamcdn-a.akamaihd.net/steam/apps/466560/ss_0de6fea3166a04027b671e438645e98186095c63.jpg?t=1571828499');
 
 END$$;
 -- +migrate StatementEnd
