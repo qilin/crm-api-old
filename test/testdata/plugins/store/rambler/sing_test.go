dfccdb514140c72274176aed7ceca33bb93c5305
Aleksandr.Barsukov <irioth@gmail.com>
2019-11-19 14:07:35 +0300

    improve url sign

--- test/testdata/plugins/store/rambler/sing_test.go
+++ test/testdata/plugins/store/rambler/sing_test.go
@@ -17,6 +17,17 @@ func TestIframeSignature(t *testing.T) {
 	}
 }
 
+func TestIframeSignatureWithoutKeys(t *testing.T) {
+	u, err := url.Parse("https://localhost/path?user_id=1683086&slug=protocolone&game_id=1079&timestamp=1573646629488&sig=89bda2b84d827138e934c529384a7b6c")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ok := VerifySignature(u.Query(), "6f12ff821d49e386c0918415322d0b74")
+	if !ok {
+		t.Error("signature not match")
+	}
+}
+
 func TestGetItemSignature(t *testing.T) {
 	u, err := url.Parse("https://localhost/some?item=100500&app_id=1079&user_id=1683086&receiver_id=1683086&lang=ru_RU&notification_type=get_item&sig=58b41bb2565f7e2b9626bd21f3691e4d")
 	if err != nil {
@@ -28,5 +39,3 @@ func TestGetItemSignature(t *testing.T) {
 		t.Error("signature not match")
 	}
 }
-
-// /integration/game/billing?item=100500&app_id=1079&user_id=1683086&receiver_id=1683086&lang=ru_RU&notification_type=get_item&sig=58b41bb2565f7e2b9626bd21f3691e4d
