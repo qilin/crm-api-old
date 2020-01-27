3828a8f50103df9c32563270fa576d77349ba2e6
Serge <serge@cheebo.me>
2020-01-27 12:55:23 +0500

    fix: fixed public.users table

--- assets/migrations/20200124173128-users.sql
+++ assets/migrations/20200124173128-users.sql
@@ -3,12 +3,16 @@
 CREATE TABLE IF NOT EXISTS public.users
 (
     id         serial      NOT NULL,
-    tenant_id  INT         NOT NULL,
-    status     boolean     NOT NULL,
+    tenant_id  INT         DEFAULT 1,
+    status     boolean     DEFAULT true,
     email      text        NOT NULL,
-    picture    text        NOT NULL,
-    first_name varchar     NOT NULL,
-    last_name  varchar     NOT NULL,
+    picture    text        DEFAULT '',
+    first_name varchar     DEFAULT '',
+    last_name  varchar     DEFAULT '',
+    role       varchar     DEFAULT '',
+    external_id varchar DEFAULT '',
+    password    varchar DEFAULT '',
+    auth_timestamp timestamptz NOT NULL DEFAULT now(),
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
     PRIMARY KEY (id)
